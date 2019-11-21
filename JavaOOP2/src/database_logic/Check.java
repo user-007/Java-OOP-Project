718e1c89a156c04e3d0b8df0171652eaddd52bc3
Yoan Ivanov <wedncv777@gmail.com>
2019-11-21 23:34:37 +0200

    Add files via upload

--- JavaOOP2/src/database_logic/Check.java
+++ JavaOOP2/src/database_logic/Check.java
@@ -1,10 +1,8 @@
-/*
 package database_logic;
 import javafx.fxml.FXML;
 import javafx.scene.control.Control;
 import sample.Controller;
 import javafx.scene.control.Button;
-import javax.xml.soap.SOAPPart;
 import javax.xml.transform.Result;
 import java.sql.*;
 import java.util.Properties;
@@ -21,24 +19,21 @@ public class Check {
 
     public boolean actionCheck() {
         //Boolean exists = false;
-       PreparedStatement pstmt = null;
+        Statement stmt = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
             //System.out.println("sdfsf");
             Connection conn = DriverManager.getConnection("jdbc:mysql://remotemysql.com/CXcocPWj6l", "CXcocPWj6l", "czNrEV9umD");
             System.out.println("Connection established");
             try{
-
-               String query  = "select ? from User where password = ?";
-                pstmt = conn.prepareStatement(query);
-                pstmt.setString(1,usr);
-                pstmt.setString(2,pass);
-                ResultSet rez = pstmt.executeQuery();
-               if(rez.next()){
-                 return true;
+                stmt = conn.createStatement();
+               String query  = "select"+usr +"from User where PassWord ="+ pass;
+               ResultSet rez = stmt.executeQuery(query);
+               if(!rez.next()){
+                 return false;
                }
                else {
-                   return false;
+                   return true;
                }
             }
             catch (SQLException excep){
@@ -51,4 +46,5 @@ public class Check {
         return false;
      }
     }
-*/
+
+
--- JavaOOP2/src/sample/UserW.java
+++ JavaOOP2/src/sample/UserW.java
@@ -1,56 +1,5 @@
 package sample;
-import javafx.application.Platform;
-import java.awt.*;
 
 public class UserW {
-    private String id;
-    private String name;
-    private String username;
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public String getDatte() {
-        return datte;
-    }
-
-    public String getIsDeleted() {
-        return isDeleted;
-    }
-
-    private String password;
-    private String role;
-    private String datte;
-    private String isDeleted;
-
-    public UserW(String id, String name, String username, String password, String role, String datte, String isDeleted) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.password = password;
-        this.role = role;
-        this.datte = datte;
-        this.isDeleted = isDeleted;
-    }
-    public Button exitOpt;
-
-
 
 }
